12fd3ee09309e3f3e7c259fe07f650481660d7ac
Vicky McDermott <vickymmcd@gmail.com>
2017-09-19 12:20:44 -0400

    adding sweep capabilities

--- sensor_servo/sensor_servo.ino
+++ sensor_servo/sensor_servo.ino
@@ -7,13 +7,18 @@ and safer since variables have scope.
 #include <Servo.h>
 
 Servo myservo;
+Servo myservo2;
 const byte sensor_pin = A0;
 const byte CMD_READ_SENSOR = 1;
-const byte CMD_MOVE_SERVO = 2;
+const byte CMD_MOVE_SERVO1 = 2;
+const byte CMD_MOVE_SERVO2 = 3;
+
+unsigned long previousMillis = 0;        // will store last time we updated
 
 long prev_t = 0;
 int sensor_value = 0;
 int pos = 0;
+int pos2 = 0;
 byte cmd_id = 0; 
 
 String result="";
@@ -21,7 +26,8 @@ String distance="";
 
 void setup() {
   //Setup input and outputs
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(10);  // attaches the servo on pin 9 to the servo object
+  myservo2.attach(9);
   Serial.begin(9600);
 }
 
@@ -31,7 +37,8 @@ void loop() {
 
   if(Serial.available() > 0) {
     cmd_id = Serial.read();
-  } else {
+  }
+  else{
     cmd_id = 0;
   }
   
@@ -44,21 +51,50 @@ void loop() {
       result = "";
       distance = "";
       break;
-    case CMD_MOVE_SERVO:
+    case CMD_MOVE_SERVO1:
       result = result + "Moving the servo now";
       Serial.println(result);
       for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
         // in steps of 1 degree
         myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        myservo2.write(pos);
         delay(15);                       // waits 15ms for the servo to reach the position
       }
       for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
         myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        myservo2.write(pos);
         delay(15);                       // waits 15ms for the servo to reach the position
+      }
+      result = "";
+      break;
+    case CMD_MOVE_SERVO2:
+      result = result + "Sweeping the servos now";
+      Serial.println(result);
+      for (pos = 0; pos <= 50; pos += 5) { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+          myservo.write(pos); // tell servo to go to position in variable 'pos'
+          delay(15);
+        for (pos2=70; pos2<=170; pos2+=1){
+            myservo2.write(pos2);
+            delay(15);
+        }
       }
       result = "";
       break;
     break;
   }
 }
+
+// If the full interval has occured, return true
+bool waitNoDelay(long millisToWait){
+  unsigned long currentMillis = millis();
+      if (currentMillis - previousMillis >= millisToWait) {
+        // save the last time you blinked the LED
+        previousMillis = currentMillis;
+        return true;
+      }
+      else{
+        return false;
+      }
+  }
 
